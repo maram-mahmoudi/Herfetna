58454ea4513133af2b45fee004e0727c1ef26563
maram-mahmoudi <marammahmoudi@aucegypt.edu>
2023-04-26 00:53:54 -0400

    modified product.php

--- product.php
+++ product.php
@@ -53,14 +53,14 @@ function cartElement($handcraft_image_link, $handcraft_type, $handcraft_price, $
                                 <h5 class=\"pt-2\">$handcraft_type</h5>
                                 <small class=\"text-secondary\">Seller: dailytuition</small>
                                 <h5 class=\"pt-2\">$$handcraft_price</h5>
-                                <button type=\"submit\" class=\"btn btn-warning\">Save for Later</button>
+                               
                                 <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Remove</button>
                             </div>
                             <div class=\"col-md-3 py-5\">
                                 <div>
-                                    <button type=\"button\" class=\"btn bg-light border rounded-circle\"><i class=\"fas fa-minus\"></i></button>
+                                    
                                     <input type=\"text\" value=\"1\" class=\"form-control w-25 d-inline\">
-                                    <button type=\"button\" class=\"btn bg-light border rounded-circle\"><i class=\"fas fa-plus\"></i></button>
+                                    
                                 </div>
                             </div>
                         </div>
