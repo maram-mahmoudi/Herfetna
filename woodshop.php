2809ce6bc6238431b7f217d6484b689fcd23761a
maram-mahmoudi <marammahmoudi@aucegypt.edu>
2023-04-26 09:14:43 -0400

    fixed file names in wrong spots

--- woodshop.php
+++ woodshop.php
@@ -13,7 +13,7 @@
             
             if (in_array($_POST['handcraft_id'], $item_array_id)){
                 echo"<script>alert('Handcraft already added to the cart!') </script>";
-                echo"<script>window.location = 'Shop.php'</script>"; 
+                echo"<script>window.location = 'woodshop.php'</script>"; 
             }else{
                 $count= count($_SESSION['cart']);
                 $item_array = array('handcraft_id'=> $_POST['handcraft_id']);
@@ -36,7 +36,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Glass Shopping</title>
+    <title> wood Shopping</title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
